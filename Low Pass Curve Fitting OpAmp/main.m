e935858abddd979a12a146c0fb07fc8f19356a34
anandpatel23 <ap947@drexel.edu>
2015-10-05 16:12:50 -0400

    cleanup

--- Low Pass Curve Fitting OpAmp/main.m
+++ Low Pass Curve Fitting OpAmp/main.m
@@ -1,3 +1,6 @@
+%% Anand Patel 
+%% Low Pass Curve Fitting OpAmp Lab
+
 %% Initialization
 clear all;
 close all;
@@ -5,13 +8,14 @@ clc;
 
 %% parase data from lab view frequency response vi
 % file with data
-file = importdata('data.txt','\t',5);
+file = importdata('data.txt', '\t', 5);
+
 % separate columns
-freq = file.data(:,1);
-VPP1 = file.data(:,2);
-VPP2 = file.data(:,3);
-GAIN = file.data(:,4);
-dBGain = file.data(:,5);
+freq = file.data(: , 1);
+VPP1 = file.data(: , 2);
+VPP2 = file.data(: , 3);
+GAIN = file.data(: , 4);
+dBGain = file.data(: , 5);
 
 %% initialize values of circuit
 R1 = 5000
@@ -19,24 +23,24 @@ theoreticalGain = -20
 frequency_DB = 2800
 
 % Filter resistance
-filterResistance = -theoreticalGain*R1
-capacitance = 1/(2*pi*filterResistance*frequency_DB)
-R_F = ['Rf= ', num2str(filterResistance), ' Ohms'];
-C1 = ['C= ', num2str(capacitance),' F'];
+filterResistance = -theoreticalGain * R1
+capacitance = 1/(2 * pi * filterResistance * frequency_DB)
+R_F = ['Rf = ', num2str(filterResistance), ' Ohms'];
+C1 = ['C = ', num2str(capacitance),' F'];
 
 % display filter resistance and capacitance
 disp(R_F)
 disp(C1)
 
 %% Measured Data Graphed - See Results Section 
-semilogx(freq,dBGain)
+semilogx(freq, dBGain)
 hold on
-timeFreq = logspace(1,6,100);
-A = ((filterResistance/R1)./(sqrt(1+((2*pi*timeFreq*filterResistance*capacitance).^2))))';
+timeFreq = logspace(1, 6, 100);
+A = ((filterResistance / R1)./(sqrt( 1 + (( 2 * pi * timeFreq * filterResistance * capacitance).^2))))';
 logA = 20*log10(A);
 
 % plot frequency of time and logX
-plot(timeFreq,logA,'r')
+plot(timeFreq, logA, 'r')
 xlabel('Frequency (Hz)')
 ylabel('Gain (dB)')
 title('Low Pass Filter of OpAmp of Ideal and Measured');
@@ -46,9 +50,9 @@ grid on
 
 %% Gain of Measured Data 
 logFreq = log10(freq);
-[p, s] = polyfit(logFreq(1:30),dBGain(1:30),1);
-rSq = double(1 - (s.normr^2)/((length(logFreq(1:30)) - 1) * std(dBGain(1:30))^2))
-sigma = double(p(1) * sqrt((1/rSq - 1)/(length(logFreq(1:30)) - 2)));
+[p, s] = polyfit(logFreq(1:30), dBGain(1:30),1);
+rSq = double(1 - (s.normr^2) / ((length(logFreq(1:30)) - 1) * std(dBGain(1:30))^2))
+sigma = double(p(1) * sqrt((1 / rSq - 1)/(length(logFreq(1:30)) - 2)));
 
 % Display Data
 x = ['GAIN = ', num2str(p(2)), ' ± ', num2str(sigma), ' dB'];
--- Transfer Functions/Lab4.m
+++ Transfer Functions/Lab4.m
@@ -8,7 +8,7 @@ close all;
 
 %% Capacitor - Low Pass (a) 
 s = tf('s');
-H1 = ((2*10^7)/(s^2+12000*s+(2*10^7)));
+H1 = ((2 * 10^7) / (s^2 + 12000 * s + (2*10^7)));
 zpk(H1)
 zeroes = zero(H1)
 poles = pole(H1)
@@ -19,8 +19,9 @@ poles = pole(H1)
 freq = 100:1:1000000;
 H1_frd = frd(H1,freq)
 [H1_Resp, H1_Freq] = frdata(H1_frd, 'v')
-MagH1_dB = 20*log10(abs(H1_Resp))
+MagH1_dB = 20 * log10(abs(H1_Resp))
 semilogx(H1_Freq, MagH1_dB)
+
 figure(1)
 grid on
 title('Magnitude of response of H(dB) -> Capacitor - Low Pass (a)', 'fontsize', 16)
@@ -29,7 +30,7 @@ ylabel('H, dB', 'fontsize', 12)
 
 %% Capacitor - Low Pass (b) 
 s = tf('s');
-H2 = ((2*10^7)/(s^2+1200*s+(2*10^7)));
+H2 = ((2 * 10^7) / (s^2 + 1200 * s + (2*10^7)));
 zpk(H2)
 zeroes = zero(H2)
 poles = pole(H2)
@@ -40,8 +41,9 @@ poles = pole(H2)
 freq = 100:1:1000000;
 H2_frd = frd(H2, freq)
 [H2_Resp, H2_Freq] = frdata(H2_frd, 'v')
-MagH2_dB = 20*log10(abs(H2_Resp))
+MagH2_dB = 20 * log10(abs(H2_Resp))
 semilogx(H2_Freq, MagH2_dB)
+
 figure(1)
 grid on
 title('Magnitude of response of H(dB) -> Capacitor - Low Pass (b)', 'fontsize', 16)
@@ -50,7 +52,7 @@ ylabel('H, dB', 'fontsize', 12)
 
 %% Inductor - High Pass (a) 
 s = tf('s');
-H3 = ((s^2)/(s^2+12000*s+(2*10^7)));
+H3 = ((s^2) / (s^2 + 12000 * s + (2 * 10^7)));
 zpk(H3)
 zeroes = zero(H3)
 poles = pole(H3)
@@ -61,8 +63,9 @@ poles = pole(H3)
 freq = 100:1:1000000;
 H3_frd = frd(H3,freq)
 [H3_Resp, H3_Freq] = frdata(H3_frd, 'v')
-MagH3_dB = 20*log10(abs(H3_Resp))
+MagH3_dB = 20 * log10(abs(H3_Resp))
 semilogx(H3_Freq, MagH3_dB)
+
 figure(1)
 grid on
 title('Magnitude of response of H(dB) -> Inductor - High Pass (a)', 'fontsize', 16)
@@ -71,7 +74,7 @@ ylabel('H, dB', 'fontsize', 12)
 
 %% Inductor - High Pass (b) 
 s = tf('s');
-H4 = ((s^2)/(s^2+1200*s+(2*10^7)));
+H4 = ((s^2) / (s^2 + 1200 * s + (2 * 10^7)));
 zpk(H4)
 zeroes = zero(H4)
 poles = pole(H4)
@@ -82,8 +85,9 @@ poles = pole(H4)
 freq = 100:1:1000000;
 H4_frd = frd(H4, freq)
 [H4_Resp, H4_Freq] = frdata(H4_frd, 'v')
-MagH4_dB = 20*log10(abs(H4_Resp))
+MagH4_dB = 20 * log10(abs(H4_Resp))
 semilogx(H4_Freq, MagH4_dB)
+
 figure(1)
 grid on
 title('Magnitude of response of H(dB) -> Inductor - High Pass (b)', 'fontsize', 16)
